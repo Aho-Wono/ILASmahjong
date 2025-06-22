04ac967562826bb2cad7882d815c7795db790364
河原佑樹 <kawahara.yuki.42f@st.kyoto-u.ac.jp>
2025-06-22 19:04:09 +0900

    三槓子と四槓子追加

---
+++ sukantu.py
@@ -0,0 +1,15 @@
+def sukantu(menzen_pattern, naki, tumo, kawa):
+    nakiseiri = []
+    for k in naki:
+        for l in range(len(k)):
+            k[l] = k[l][0]
+        nakiseiri.append(k)
+    tehaikari = menzen_pattern + nakiseiri
+    hantei = 0
+    for i in tehaikari:
+        if len(i) == 4:
+            hantei += 1
+    if hantei == 4:
+        return True
+    else:
+        return False
