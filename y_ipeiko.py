b7fec7a4c5cfd19f08b5c9271198b0c4a53f8a48
Sug <sugar77.xe@gmail.com>
2025-06-30 17:07:38 +0900

    今日は5限登校

--- y_ipeiko.py
+++ y_ipeiko.py
@@ -1,9 +1,13 @@
 import itertools
-
-def ipeiko(menzen_pattern, naki, kawa, tumo, agarihai):
-    if not naki:
-        return False
+import y_ryanpeiko
+def y_ipeiko(PlayerInfo, menzen_pattern, agarihai):
+    naki = PlayerInfo.tehai["naki"]
+    for naki_i in naki:
+        flag = naki_i[0][1] #あんかんのとき鳴いたもののリストの誰のものか(2番目)がすべて一致するはずなので、違うときにFalse
+        for item in naki_i:
+            if item[1] != flag:
+                return False
     for item in itertools.combinations(menzen_pattern, 2):
         if item[0] == item[1]:
-            return True
-    return False
+            return "一盃口"
+    return False 
