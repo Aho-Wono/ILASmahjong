034614201f8ea011de60a349a7f54d5877afd41c
Sug <sugar77.xe@gmail.com>
2025-06-30 17:21:21 +0900

    頭壊れる

--- y_tintumo.py
+++ y_tintumo.py
@@ -1,4 +1,9 @@
 def y_tintumo(menzen_pattern, naki, tumo, kawa):
     if naki:
         return True
-    return False #あってるんか？
+    for naki_i in naki:
+        flag = naki_i[0][1]
+        for item in naki_i:
+            if flag != item[1]:
+                return False
+    return True #あってるんか？→あってるよ
