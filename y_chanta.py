a7ce425b2aa7b9350013cf3de538785db4fc59e3
Sug <sugar77.xe@gmail.com>
2025-06-30 17:56:01 +0900

    チキータ

---
+++ y_chanta.py
@@ -0,0 +1,28 @@
+def y_chanta(PlayerInfo, menzen_pattern, agarihai):
+    naki = PlayerInfo.tehai["naki"]
+    for menz in menzen_pattern:
+        flag = True
+        for item in menz:
+            try:
+                if (len(item) == 2):
+                    i = int(item[1])
+                    if i == 1 or i == 9:
+                        flag = False
+                else:
+                    flag = False
+            except Exception as e:
+                print(e)
+        if flag:
+            return False
+    for naki_i in naki:
+        flag = True
+        for item in naki_i:
+            if(len(item[0]) == 2):
+                i = int(item[1])
+                if i == 1 or i == 9:
+                    flag = False
+            else: 
+                flag = False
+        if flag:
+            return False
+    return "混全帯么九"
