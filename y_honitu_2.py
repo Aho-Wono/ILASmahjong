c57c447971915058ba4cfb06a193aea7a187f14b
Sug <sugar77.xe@gmail.com>
2025-07-06 14:17:03 +0900

    ほんいつの食い下がり修正した

---
+++ y_honitu_2.py
@@ -0,0 +1,22 @@
+def y_honitu_2(PlayerInfo, menzen_pattern, agarihai):
+    if not PlayerInfo.ifnaki():
+        return False
+    naki = PlayerInfo.tehai["naki"]
+    m_char = 'x'
+    for menz in menzen_pattern:
+        for item in menz:
+            if len(item) == 2:
+                if m_char == 'x':
+                    m_char = item[0]
+                else:
+                    if m_char != item[0]:
+                        return False
+    for menz in naki:
+        for item in menz:
+            if len(item[0]) == 2:
+                if m_char == 'x':
+                    m_char = item[0][0]
+                else:
+                    if m_char != item[0][0]:
+                        return False
+    return "混一色"
