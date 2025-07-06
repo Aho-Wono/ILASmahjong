82a01e2655a77f11f8db3e2747446f44e424efe1
Sug <sugar77.xe@gmail.com>
2025-07-06 14:23:10 +0900

    二盃口修正　だれかデバッグ頼む

--- y_ryanpeiko.py
+++ y_ryanpeiko.py
@@ -2,14 +2,15 @@ import itertools
 
 def y_ryanpeiko(PlayerInfo, menzen_pattern, agarihai):
     naki = PlayerInfo.tehai["naki"]
-    for naki_i in naki:
-        flag = naki_i[0][1] #あんかんのとき鳴いたもののリストの誰のものか(2番目)がすべて一致するはずなので、違うときにFalse
-        for item in naki_i:
-            if item[1] != flag:
-                return False
+    # for naki_i in naki:
+    #    flag = naki_i[0][1] #あんかんのとき鳴いたもののリストの誰のものか(2番目)がすべて一致するはずなので、違うときにFalse
+    #    for item in naki_i:
+    #        if item[1] != flag:
+    #            return False
     if len(menzen_pattern) != 4:
         return False
     for permu in itertools.permutations(menzen_pattern, 4):
-        if permu[0] == permu[1] and permu[2] == permu[3]:
+        if permu[0] == permu[1] and permu[2] == permu[3] and permu[1] != permu[2]:
             return "二盃口"
     return False
+    # あんかんがあった時点でりゃんぺは成立しないので、コメントアウトした5行がおそらくいらない
