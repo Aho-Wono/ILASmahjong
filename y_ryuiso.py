1b0adadfba1121f05b1145f4f72c609cbb1ca732
河原佑樹 <kawahara.yuki.42f@st.kyoto-u.ac.jp>
2025-07-07 16:46:27 +0900

    緑一色追加

---
+++ y_ryuiso.py
@@ -0,0 +1,15 @@
+def y_ryuiso(PlayerInfo, menzen_pattern, agarihai):
+    naki = PlayerInfo.tehai["naki"]
+    nakiseiri = []
+    for k in naki:
+        for l in range(len(k)):
+            k[l] = k[l][0]
+        nakiseiri.append(k)
+    tehaikari = menzen_pattern + nakiseiri
+    kensa = []
+    for i in tehaikari:
+        kensa = kensa + i
+    for j in kensa:
+        if not j in ("s2" , "s3" , "s4" , "s6" , "s8" , "hatu"):
+            return False
+    return "緑一色"
