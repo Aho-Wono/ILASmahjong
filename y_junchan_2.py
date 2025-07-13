4360e1a24666084ced4e4bcc079a4a0fb3e4224b
Sug <sugar77.xe@gmail.com>
2025-07-13 22:21:28 +0900

    じゅんちゃん

---
+++ y_junchan_2.py
@@ -0,0 +1,25 @@
+def y_junchan_2(PlayerInfo, menzen_pattern, agarihai):
+    if not PlayerInfo.ifnaki():
+        return False
+    if len(menzen_pattern) == 7:
+        return False
+    flag = True
+    for menz in menzen_pattern:
+        flag_1 = False
+        for item in menz:
+            if len(item) == 2:
+                if item[1] == '1' or item[1] == '9':
+                    flag_1 = True
+        flag = flag_1
+    naki = PlayerInfo.tehai["naki"]
+    for menz in naki:
+        flag_1 = False
+        for item in menz:
+            if len(item[0]) == 2:
+                if item[0][1] == '1' or item[0][1] == '9':
+                    flag_1 = True
+        flag = flag_1
+    if flag:
+        return "純全帯么九_3"
+    else:
+        return False
