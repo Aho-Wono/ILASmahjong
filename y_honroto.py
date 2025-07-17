a0da360e83bc20c5783ff611a00094b692bcd21f
河原佑樹 <kawahara.yuki.42f@st.kyoto-u.ac.jp>
2025-07-17 16:32:48 +0900

    混老頭修正と清老頭追加

--- y_honroto.py
+++ y_honroto.py
@@ -1,18 +1,19 @@
+import y_tinroto
 def y_honroto(PlayerInfo, menzen_pattern, agarihai):
+    if y_tinroto.y_tinroto(PlayerInfo, menzen_pattern, agarihai):
+        return False
     flag = True
     for menz in menzen_pattern:
         for item in menz:
-            if len(item) != 2:
-                flag = False
-            elif int(item[1]) != 1 and int(item[1]) != 9:
-                flag = False
+            if len(item) == 2:
+                if int(item[1]) != 1 and int(item[1]) != 9:
+                    flag = False
     naki = PlayerInfo.tehai["naki"]
     for menz in naki:
         for item in menz:
-            if len(item[0]) != 2:
-                flag = False
-            elif int(item[0][1]) != 1 and int(item[0][1]) != 9:
-                flag = False
+            if len(item[0]) == 2:
+                if int(item[0][1]) != 1 and int(item[0][1]) != 9:
+                    flag = False
     if flag:
         return "混老頭"
     else:
---
+++ y_tinroto.py
@@ -0,0 +1,19 @@
+def y_tinroto(PlayerInfo, menzen_pattern, agarihai):
+    flag = True
+    for menz in menzen_pattern:
+        for item in menz:
+            if len(item) != 2:
+                flag = False
+            elif int(item[1]) != 1 and int(item[1]) != 9:
+                flag = False
+    naki = PlayerInfo.tehai["naki"]
+    for menz in naki:
+        for item in menz:
+            if len(item[0]) != 2:
+                flag = False
+            elif int(item[0][1]) != 1 and int(item[0][1]) != 9:
+                flag = False
+    if flag:
+        return "清老頭"
+    else:
+        return False
