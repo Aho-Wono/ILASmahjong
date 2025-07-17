494186063c1f978f55eae11a0ec968f210ae2b44
Aho-Wono <Wono.minecraft@gmail.com>
2025-07-17 18:12:43 +0900

    かわはらいらい

--- main.py
+++ main.py
@@ -358,6 +358,17 @@ async def start_ai():
     printd("AI can do", what_ai_can_do)
     printd("START AI THINKING")
     await asyncio.sleep(0.5) # とりま待たせる
+
+    # 河原依頼
+    import chappy_choice
+    situations = {
+        "kawa_li": None,
+        "tehai": None,
+    }
+    playerinfo_li = Game.players
+    AI_cmd = chappy_choice.chappy_choice(situations=situations, what_ai_can_do= what_ai_can_do) # 待機時間が発生する関数
+
+
     AI_cmd = random.choice(what_ai_can_do)
     printd("FINISH AI THINKING")
 
