dd208d344b65b93a749260698b777c65518d934d
Sug <sugar77.xe@gmail.com>
2025-07-17 16:48:50 +0900

    しょうしーしー　なおした

--- y_shosushi.py
+++ y_shosushi.py
@@ -12,10 +12,14 @@ def y_shosushi(PlayerInfo, menzen_pattern, agarihai):
         return False
     cnt = 0
     for menz in menzen_pattern:
+        if len(menz) == 2:
+            continue
         if ifzihai(menz[0]):
             cnt += 1
     naki = PlayerInfo.tehai["naki"]
     for menz in naki:
+        if len(menz) == 2:
+            continue
         if ifzihai(menz[0][0]):
             cnt += 1
     if cnt == 3:
