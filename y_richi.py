dcb32c5343503153a11b68b8f982c8e89ca80bbe
河原佑樹 <kawahara.yuki.42f@st.kyoto-u.ac.jp>
2025-08-04 02:34:13 +0900

    ダブル立直と立直の競合を修正

--- y_richi.py
+++ y_richi.py
@@ -1,4 +1,7 @@
+import y_doublerichi
 def y_richi(PlayerInfo, menzen_pattern, agarihai):
+    if y_doublerichi.y_doublerichi(PlayerInfo, menzen_pattern, agarihai):
+        return False
     if PlayerInfo.ifrichi():
         return "立直"
     else:
