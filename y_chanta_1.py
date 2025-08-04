b6719a22608001b63b523a192cebdb94e899a747
Sug <sugar77.xe@gmail.com>
2025-08-04 13:15:55 +0900

    修正

--- y_chanta_1.py
+++ y_chanta_1.py
@@ -1,7 +1,8 @@
 import y_honroto
 import y_junchan_2
+import y_junchan_3
 def y_chanta_1(PlayerInfo, menzen_pattern, agarihai):
-    if y_honroto.y_honroto(PlayerInfo, menzen_pattern, agarihai) or y_junchan_2.y_junchan_2(PlayerInfo, menzen_pattern, agarihai) or y_junchan_1.y_junchan_1(PlayerInfo, menzen_pattern, agarihai):
+    if y_honroto.y_honroto(PlayerInfo, menzen_pattern, agarihai) or y_junchan_2.y_junchan_2(PlayerInfo, menzen_pattern, agarihai) or y_junchan_3.y_junchan_3(PlayerInfo, menzen_pattern, agarihai):
         return False
     if not PlayerInfo.ifnaki():
         return False
@@ -22,7 +23,7 @@ def y_chanta_1(PlayerInfo, menzen_pattern, agarihai):
     for naki_i in naki:
         flag = True
         for item in naki_i:
-            if(len(item[0]) == 2):
+            if len(item[0]) == 2:
                 i = int(item[0][1])
                 if i == 1 or i == 9:
                     flag = False
